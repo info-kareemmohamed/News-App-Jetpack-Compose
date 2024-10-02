cbe4acc345853ea8eb3cc469e108c98963c7d1aa
Kareem Mohamed <63722@fa-hiss.edu.eg>
2024-10-02 08:13:47 +0300

    change the color of status bar

--- app/src/main/java/com/example/news/core/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/example/news/core/presentation/ui/theme/Theme.kt
@@ -48,8 +48,9 @@ fun NewsTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            window.statusBarColor = colorScheme.background.toArgb()
+            window.navigationBarColor = colorScheme.background.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
 
