54808034058d3f185ecd401025d81734a7caad94
Kareem Mohamed <63722@fa-hiss.edu.eg>
2024-10-02 06:40:14 +0300

    presentation : onboarding screen

--- app/src/main/java/com/example/news/core/presentation/MainActivity.kt
+++ app/src/main/java/com/example/news/core/presentation/MainActivity.kt
@@ -3,8 +3,13 @@ package com.example.news.core.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.news.core.presentation.ui.theme.NewsTheme
+import com.example.news.onboarding.presentation.OnBoardingScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -14,7 +19,9 @@ class MainActivity : ComponentActivity() {
         installSplashScreen()
         setContent {
             NewsTheme {
-
+                Box(modifier = Modifier.background(MaterialTheme.colorScheme.background)) {
+                    OnBoardingScreen()
+                }
             }
         }
     }
