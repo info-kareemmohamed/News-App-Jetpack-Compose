e7bec3e47e2c53f5004f0e19ce4de6c2b114f693
Kareem Mohamed <63722@fa-hiss.edu.eg>
2024-10-03 10:27:51 +0300

    data : remote : setup api

---
+++ app/src/main/java/com/example/news/home/data/remote/NewsPagingSource.kt
@@ -0,0 +1,39 @@
+package com.example.news.home.data.remote
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.example.news.home.data.mappers.toArticle
+import com.example.news.home.domain.model.Article
+
+
+class NewsPagingSource(
+    private val newsApi: NewsApi,
+    private val sources: String
+) : PagingSource<Int, Article>() {
+    override fun getRefreshKey(state: PagingState<Int, Article>): Int? {
+        return state.anchorPosition?.let { position ->
+            state.closestPageToPosition(position)?.nextKey?.minus(1) ?: 1
+        }
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Article> {
+        val page = params.key ?: 1
+        return try {
+            val newsResponse = newsApi.getNews(sources, page)
+            val articles = newsResponse.articles.map { it.toArticle() }
+            LoadResult.Page(
+                data = articles,
+                nextKey = if ((newsResponse.totalResults / page) == 0) null else page + 1,
+                prevKey = if (page == 1) null else page - 1
+            )
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+            LoadResult.Error(
+                throwable = e
+            )
+        }
+    }
+
+
+}
