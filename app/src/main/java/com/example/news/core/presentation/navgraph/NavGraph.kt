c3c7d62a0d8d134250a5d9134b60e4570b8d9c44
Kareem Mohamed <63722@fa-hiss.edu.eg>
2024-10-06 11:32:09 +0300

    bookmark screen

--- app/src/main/java/com/example/news/core/presentation/navgraph/NavGraph.kt
+++ app/src/main/java/com/example/news/core/presentation/navgraph/NavGraph.kt
@@ -1,20 +1,14 @@
 package com.example.news.core.presentation.navgraph
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
-import androidx.paging.compose.collectAsLazyPagingItems
-import com.example.news.home.presentation.HomeScreen
-import com.example.news.home.presentation.HomeViewModel
+import com.example.news.bookmark.presentation.BookmarkScreen
+import com.example.news.bookmark.presentation.BookmarkViewModel
 import com.example.news.onboarding.presentation.OnBoardingScreen
-import com.example.news.search.presentation.SearchScreen
-import com.example.news.search.presentation.mvi.SearchViewModel
 
 @Composable
 fun NavGraph(
@@ -45,12 +39,9 @@ fun NavGraph(
             composable(
                 route = Route.NewsNavigatorScreen.route
             ) {
-                val viewModel: SearchViewModel = hiltViewModel()
-                SearchScreen(
-                    state = viewModel.state.value,
-                    event = viewModel::onIntent,
-                    navigateToDetails = {}
-                )
+                val viewModel: BookmarkViewModel = hiltViewModel()
+               BookmarkScreen(articles = viewModel.articles.value,{})
+
             }
         }
 
