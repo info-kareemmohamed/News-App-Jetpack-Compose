a1468dfc220a91ebf716353a8ce22266405f2e0a
Kareem Mohamed <63722@fa-hiss.edu.eg>
2024-10-07 03:24:34 +0300

    move local data base to core

--- app/src/main/java/com/example/news/core/domain/repository/NewsRepository.kt
+++ app/src/main/java/com/example/news/core/domain/repository/NewsRepository.kt
@@ -5,6 +5,15 @@ import com.example.news.core.domain.model.Article
 import kotlinx.coroutines.flow.Flow
 
 interface NewsRepository {
-     fun getNews(sources: List<String>): Flow<PagingData<Article>>
-     fun searchForNews(sources: List<String>,searchQuery: String): Flow<PagingData<Article>>
+    fun getNews(sources: List<String>): Flow<PagingData<Article>>
+
+    fun searchForNews(sources: List<String>, searchQuery: String): Flow<PagingData<Article>>
+
+    suspend fun upsert(article: Article)
+
+    suspend fun delete(article: Article)
+
+    suspend fun getArticleByUrl(url: String): Article?
+
+    fun getBookMarkedArticles(): Flow<List<Article>>
 }
